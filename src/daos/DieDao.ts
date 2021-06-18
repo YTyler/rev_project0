5e48231c4d12f8199c4104cc77406a82fcb945ff
Tyler C. Yates <tyates907@gmail.com>
2021-06-18 16:24:13 -0700

    finish function to read one die object

--- src/daos/DieDao.ts
+++ src/daos/DieDao.ts
@@ -1,19 +1,36 @@
 import { DieIF } from '../entities/Die';
+import { ddbClient } from "../../db/ddbClient";
+import { GetItemCommand, GetItemCommandInput } from '@aws-sdk/client-dynamodb';
+
 
 export interface DieDaoIF {
     getAll: () => Promise<DieIF[]>;
-    getOne: (id: number) => Promise<DieIF | null>;
+    getOne: (id: string) => Promise<DieIF | null>;
 }
 
 export default class DieDao implements DieDaoIF {
-
+    private TABLE_NAME = "DICE_TABLE"
     public getAll(): Promise<DieIF[]> {
         
         return Promise.resolve([])
     }
 
-    public getOne(id: number): Promise<DieIF> | null {
-        
+    public getOne(id: string): Promise<DieIF> | null {
+        // Set the parameters
+        const params: GetItemCommandInput = {
+            TableName: this.TABLE_NAME,
+            Key: {
+                KEY_NAME: { N: id},
+            },
+        };
+        const run = async () => {
+            const data = await ddbClient.send(new GetItemCommand(params));
+            console.log("Success", data.Item);
+            return data;
+        };
+
+
+
         return Promise.resolve(null);
     }
 }
